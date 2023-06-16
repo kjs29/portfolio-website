d5fdfbc21f5037d74c71f262e1bb6b7acbd20f53
Jinsung <jsk.jinsung@gmail.com>
2023-06-15 19:36:18 -0600

    added typewriter effect, minimize, maximize functionalities

--- resources/index.js
+++ resources/index.js
@@ -1,6 +1,60 @@
 
+// typewriter effect
+async function typeAll() {
+    let texts = document.querySelectorAll('.terminal-middle p');
+    
+    for(let textNode of texts) {
+        let text = textNode.innerText;
+        textNode.style.opacity = 1;
+        textNode.innerHTML = '';
+        await type(textNode, text);
+    }
+}
+
+function type(textNode, text) {
+    return new Promise((resolve, reject) => {
+        let i = 0;
+
+        let typing = setInterval(() => {
+            if (i < text.length) {
+                textNode.innerHTML += text.charAt(i);
+                i++;
+            } else {
+                clearInterval(typing);
+                resolve();
+            }
+        }, 50);
+    });
+}
+
+typeAll();
+
+
+// add round button 
 window.onload = function() {
     const title = document.querySelector('title').innerText;
     let myComputer = document.querySelector('.terminal-command p:last-child');
     myComputer.appendChild(document.createTextNode(title+' % '));
-}   
+}
+
+// hide code editor on clicking yellow-dot
+document.querySelector('div.yellow-dot').addEventListener('click', function() {
+    document.querySelector('.wrapper').style.display = 'none';
+});
+
+// maximizing window on clicking green-dot
+document.querySelector('div.green-dot').addEventListener('click', function() {
+    document.querySelector('body').style.maxWidth = '100%';
+    document.querySelector('body').style.margin = '0';
+    document.querySelector('body').style.width = '100%';
+});
+
+// right click event
+// document.addEventListener('contextmenu', function(ev) {
+//     let wrapper = document.getElementById('editor');
+//     if (!wrapper.contains(ev.target)) {
+//         ev.preventDefault();
+//         alert('success!');
+//         return false;
+//     }
+// }, false);
