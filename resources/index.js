c7c1ab09b0589f96f176111a6dc35d8a5cceb66e
Jinsung <jsk.jinsung@gmail.com>
2023-06-24 12:55:37 -0600

    refactored index.js removing global queries/ modified url for resume dblclicking functionality

--- resources/index.js
+++ resources/index.js
@@ -1,8 +1,8 @@
 
 // drag and drop functionality
-
 let vsCodeWindow = document.querySelector('.wrapper');
-let topWindow = document.querySelector('.top');
+let topWindow = vsCodeWindow.querySelector('.top');
+let mainWindow = vsCodeWindow.querySelector('main')
 
 topWindow.addEventListener('mousedown', function (e) {
     const offsetX = e.clientX - vsCodeWindow.offsetLeft;
@@ -26,20 +26,18 @@ window.onload = function() {
     myComputer.appendChild(document.createTextNode(title+' % '));
 }
 
-// hide code editor on clicking red-dot
-document.querySelector('div.red-dot').addEventListener('click', function() {
-    // vsCodeWindow.classList.add('minimize');
+// hide vsCodeWindow on clicking red-dot
+topWindow.querySelector('div.red-dot').addEventListener('click', function() {
     vsCodeWindow.style.display = 'none';
 });
 
-// hide code editor on clicking yellow-dot
-document.querySelector('div.yellow-dot').addEventListener('click', function() {
-    // vsCodeWindow.classList.add('minimize');
+// hide vsCodeWindow on clicking yellow-dot
+topWindow.querySelector('div.yellow-dot').addEventListener('click', function() {
     vsCodeWindow.style.display = 'none';
 });
 
 // maximizing window on clicking green-dot
-vsCodeWindow.querySelector('div.green-dot').addEventListener('click', function() {
+topWindow.querySelector('div.green-dot').addEventListener('click', function() {
     vsCodeWindow.classList.toggle('maxSize');
 });
 
@@ -102,22 +100,22 @@ folders.forEach(folder => {
 document.addEventListener('click', resetFolderStyles);
 
 // when portfolio folder is double clicked, its display is reset
-document.querySelector('#my-portfolio').addEventListener('dblclick', function() {
+document.querySelector('#portfolio').addEventListener('dblclick', function() {
     vsCodeWindow.style.display = 'unset';
 });
 
 // when Resume.pdf is double clicked, it displays resume in a new tab
 document.querySelector('#resume').addEventListener('dblclick', () => {
-    window.open('./resume.pdf', '_blank');
+    window.open('https://kjs29.github.io/portfolio-website/resources/resume.pdf', '_blank');
 });
 
 /**********************************************************/
 
 // typewriter effect
 async function typeAll() {
-    let texts = document.querySelectorAll('.terminal-middle p');
+    let texts = mainWindow.querySelectorAll('.terminal-middle p');
     
-    for(let textNode of texts) {
+    for (let textNode of texts) {
         let text = textNode.innerText;
         textNode.style.opacity = 1;
         textNode.innerHTML = '';
@@ -151,10 +149,10 @@ let rightClickMenuProjects = document.querySelector('.menu-container p:nth-child
 let rightClickMenuContact = document.querySelector('.menu-container p:nth-child(4)');
 let rightClickMenuDisableMenu = document.querySelector('.menu-container p:last-child');
 
-let navLinkFirst = document.querySelector('nav li:nth-child(1)');
-let navLinkSecond = document.querySelector('nav li:nth-child(2)');
-let navLinkThird = document.querySelector('nav li:nth-child(3)');
-let navLinkFourth = document.querySelector('nav li:nth-child(4)');
+let navLinkFirst = mainWindow.querySelector('nav li:nth-child(1)');
+let navLinkSecond = mainWindow.querySelector('nav li:nth-child(2)');
+let navLinkThird = mainWindow.querySelector('nav li:nth-child(3)');
+let navLinkFourth = mainWindow.querySelector('nav li:nth-child(4)');
 
 rightClickMenuDisableMenu.addEventListener('click', () => {
     document.removeEventListener('contextmenu', disableContextMenu);
@@ -194,31 +192,31 @@ navLinkFourth.addEventListener('click', ()=> {
 
 function scrollToAboutMe() {
     vsCodeWindow.style.display = 'block';
-    const targetElement = document.querySelector('#about-me');
+    const targetElement = mainWindow.querySelector('#about-me');
     targetElement.scrollIntoView({behavior: 'smooth'});
 }
 
 function scrollToSkills() {
     vsCodeWindow.style.display = 'block';
-    const targetElement = document.querySelector('#skills');
+    const targetElement = mainWindow.querySelector('#skills');
     targetElement.scrollIntoView({behavior: 'smooth'});
 }
 
 function scrollToProjects() {
     vsCodeWindow.style.display = 'block';
-    const targetElement = document.querySelector('#projects');
+    const targetElement = mainWindow.querySelector('#projects');
     targetElement.scrollIntoView({behavior: 'smooth'});
 }
 
 function scrollToContact() {
     vsCodeWindow.style.display = 'block';
-    const targetElement = document.querySelector('#contact');
+    const targetElement = mainWindow.querySelector('#contact');
     targetElement.scrollIntoView({behavior: 'smooth'});
 }
 
 // change active class as page scrolls
-let navigationMenu = document.querySelectorAll('nav ul li');
-let output = document.querySelector('.output');
+let navigationMenu = mainWindow.querySelectorAll('nav ul li');
+let output = mainWindow.querySelector('.output');
 
 output.addEventListener('scroll', () => {
     
